3b671b347c2d32838bbd213c94badc6895d54cc0
Sina Matian <89218912+smatian@users.noreply.github.com>
2024-05-27 21:11:06 +0000

    added redirect to /instagram if you go to the index page

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect
 from config import Config
 from blueprints.instagram import instagram_blueprint
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(instagram_blueprint, url_prefix='/instagram')
+
+@app.route("/")
+def instaRedirect():
+    return redirect("/instagram")
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
